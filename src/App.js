fe41778828ad454ec8ad53293061093b4affb120
divyam-cogito <dbohra@cogitocorp.com>
2023-08-23 11:26:09 +0530

    fix(app.js): fixed a failing test case

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Updated
+          Updated React App
         </a>
       </header>
     </div>
